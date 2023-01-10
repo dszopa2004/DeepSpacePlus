c63357e37c3d68b3e87aad4a55ddecc86e57f936
Dominik Szopa <55515534+dszopa2004@users.noreply.github.com>
2023-01-10 12:30:49 -0600

    add force_block functionality
    gave force block the ability to push player away when player touches a specific corner of block (must fix: make it push away on any side touched)
    referred to the class of the cactus block (ctrl + N to view mc source code)

--- src/main/java/com/snoops35/deepspaceplus/core/ForceBlock.java
+++ src/main/java/com/snoops35/deepspaceplus/core/ForceBlock.java
@@ -9,11 +9,20 @@ import net.minecraft.block.material.EnumPushReaction;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
 
 public class ForceBlock extends Block implements IHasModel
 {
+    public static final AxisAlignedBB FORCE_COLLISION_AABB = new AxisAlignedBB(0.001,0.001,0.001,0.999,0.999,0.999);
+
     public ForceBlock(String name, Material material)
     {
         super(material);
@@ -28,18 +37,27 @@ public class ForceBlock extends Block implements IHasModel
     }
 
     @Override
-    public boolean isFullBlock(IBlockState state) {
-        return true;
+    public boolean isFullCube(IBlockState state) {
+        return false;
     }
 
     @Override
-    public boolean isFullCube(IBlockState state) {
-        return true;
-    }
+    public boolean isOpaqueCube(IBlockState state) { return false; }
 
     @Override
     public EnumPushReaction getMobilityFlag(IBlockState state) {
         return EnumPushReaction.BLOCK;
+    }
+
+    @Nullable
+    @Override
+    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
+        return FORCE_COLLISION_AABB;
+    }
+
+    @Override
+    public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn) {
+        entityIn.addVelocity(1,1,1);
     }
 
     @Override
