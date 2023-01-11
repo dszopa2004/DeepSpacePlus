931127c682c19deb640266818f2bf867d2ee30d4
Dominik Szopa <55515534+dszopa2004@users.noreply.github.com>
2023-01-11 12:59:08 -0600

    change pushback
    the push back on the player no longer depends on the direction they are facing
    this code doesnt push the player back, but prevents them from going forward

--- src/main/java/com/snoops35/deepspaceplus/core/ForceBlock.java
+++ src/main/java/com/snoops35/deepspaceplus/core/ForceBlock.java
@@ -10,9 +10,9 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -22,7 +22,7 @@ import javax.annotation.Nullable;
 
 public class ForceBlock extends Block implements IHasModel
 {
-    public static final AxisAlignedBB FORCE_COLLISION_AABB = new AxisAlignedBB(1,1,1,1,1,1);
+    public static final AxisAlignedBB FORCE_COLLISION_AABB = new AxisAlignedBB(2,2,2,2,2,2);
 
     public ForceBlock(String name, Material material)
     {
@@ -57,25 +57,13 @@ public class ForceBlock extends Block implements IHasModel
     @Override
     public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn)
     {
-        EnumFacing playerFacing = entityIn.getHorizontalFacing();
-        double pushBack = 5;
-        double height = 1;
-        switch(playerFacing) {
-            case EAST:
-                entityIn.addVelocity(-pushBack, height, 0);
-                break;
-            case WEST:
-                entityIn.addVelocity(pushBack, height, 0);
-                break;
-            case NORTH:
-                entityIn.addVelocity(0, height, pushBack);
-                break;
-            case SOUTH:
-                entityIn.addVelocity(0, height, -pushBack);
-                break;
-        }
-    }
+        if (!(entityIn instanceof EntityPlayer)) return;
+        EntityPlayer player = (EntityPlayer) entityIn;
+        double pushAmount = 1;
+        double height = 0.5;
 
+        player.addVelocity(-player.motionX * pushAmount, height, -player.motionZ * pushAmount);
+    }
 
     @Override
     public void registerModels() { DeepSpacePlus.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory"); }
