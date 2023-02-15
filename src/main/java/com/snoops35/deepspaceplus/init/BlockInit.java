0c161772f304d87f08d6f4a54da8b4ad83db728c
Dominik Szopa <55515534+dszopa2004@users.noreply.github.com>
2023-02-15 15:33:34 -0600

    added mud block

--- src/main/java/com/snoops35/deepspaceplus/init/BlockInit.java
+++ src/main/java/com/snoops35/deepspaceplus/init/BlockInit.java
@@ -3,6 +3,7 @@ package com.snoops35.deepspaceplus.init;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.snoops35.deepspaceplus.blocks.BlockBase;
 import com.snoops35.deepspaceplus.blocks.BlockFluid;
 import com.snoops35.deepspaceplus.blocks.ForceBlock;
 import net.minecraft.block.Block;
@@ -13,6 +14,7 @@ public class BlockInit
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
 	
 	public static final Block FORCE_BLOCK = new ForceBlock("force_block", Material.GROUND);
+	public static final Block MUD_BLOCK = new BlockBase("mud", Material.GROUND);
 	public static final Block AMBROSIA_BLOCK = new BlockFluid("ambrosia", FluidInit.AMBROSIA, Material.WATER);
 	public static final Block CAELUM_BLOCK = new BlockFluid("caelum", FluidInit.CAELUM, Material.WATER);
 }
